8062d0a0d58ffd491a30c645fbc49dfaaed16b2f
Kostis-S-Z <kostissz@pm.me>
2025-12-03 16:14:47 +0200

    Update e2e test for TEST_API_KEY & test dataset ID

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -7,20 +7,21 @@ from _pytest.monkeypatch import MonkeyPatch
 
 from datacollective import get_dataset_details, load_dataset
 
-MDC_API_KEY = os.getenv("MDC_API_KEY")
+MDC_TEST_API_KEY = os.getenv("MDC_TEST_API_KEY")
 MDC_TEST_API_URL = os.getenv("MDC_TEST_API_URL")
 
 pytestmark = pytest.mark.skipif(
-    not (MDC_API_KEY and MDC_TEST_API_URL),
+    not (MDC_TEST_API_KEY and MDC_TEST_API_URL),
     reason="Set MDC_API_KEY and MDC_TEST_API_URL to run live API tests.",
 )
 
 
 def test_get_dataset_details_live_api(
     monkeypatch: MonkeyPatch,
-    dataset_id: str = "cmhvzlidq0326mn07hk4do3pj",
+    dataset_id: str = "cmiq2s3q5000fo207k9g6g7ou",
 ) -> None:
     """NOTE: This test calls a live MDC API endpoint (dev)."""
+    monkeypatch.setenv("MDC_API_KEY", MDC_TEST_API_KEY)
     monkeypatch.setenv("MDC_API_URL", MDC_TEST_API_URL)
 
     details = get_dataset_details(dataset_id)
@@ -33,10 +34,11 @@ def test_get_dataset_details_live_api(
 def test_load_dataset_live_api(
     tmp_path: Path,
     monkeypatch: MonkeyPatch,
-    dataset_id: str = "cmhvzlidq0326mn07hk4do3pj",
+    dataset_id: str = "cmiq2s3q5000fo207k9g6g7ou",
 ) -> None:
     """NOTE: This test calls a live MDC API endpoint (dev)."""
 
+    monkeypatch.setenv("MDC_API_KEY", MDC_TEST_API_KEY)
     monkeypatch.setenv("MDC_DOWNLOAD_PATH", str(tmp_path))
     monkeypatch.setenv("MDC_API_URL", MDC_TEST_API_URL)
 
