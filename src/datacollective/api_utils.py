e863ab7ea765ea7869f89731e00995d5a9df558c
Kostis-S-Z <kostissz@pm.me>
2026-01-27 15:47:09 -0300

    Remove dotenv

--- src/datacollective/api_utils.py
+++ src/datacollective/api_utils.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from typing import Any
 
 import requests
-from dotenv import load_dotenv
 
 DEFAULT_API_URL = "https://datacollective.mozillafoundation.org/api"
 ENV_API_KEY = "MDC_API_KEY"
@@ -15,8 +14,6 @@ ENV_DOWNLOAD_PATH = "MDC_DOWNLOAD_PATH"
 HTTP_TIMEOUT = (10, 60)  # (connect, read)
 
 RATE_LIMIT_ERROR = "Rate limit exceeded. Please try again later."
-
-load_dotenv()
 
 
 def send_api_request(
