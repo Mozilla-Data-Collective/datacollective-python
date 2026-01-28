77d964a46702e56bab481582866e32c499407a84
Kostis-S-Z <kostissz@pm.me>
2026-01-28 15:43:20 -0300

    Bump version to 0.3.0

--- src/datacollective/__init__.py
+++ src/datacollective/__init__.py
@@ -7,4 +7,4 @@ from .datasets import get_dataset_details, load_dataset, save_dataset_to_disk
 __all__ = ["save_dataset_to_disk", "load_dataset", "get_dataset_details", "__version__"]
 
 # DO NOT EDIT THE VERSION MANUALLY, USE bump-my-version TO UPDATE. See release.md
-__version__ = "0.2.0"
+__version__ = "0.3.0"
